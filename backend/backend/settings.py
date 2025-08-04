22cb1e71f903ec8254ffc60a6403658b782b9795
Dheelyte <olagbujidelight@gmail.com>
2025-08-04 23:17:00 +0100

    Modify storage file settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -180,11 +180,8 @@ else:
     AWS_DEFAULT_ACL = 'public-read' # Change if your files should not be publicly accessible
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    # Tell Django to use S3 for file storage
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
     # Optional: Configure static files to also be served from S3
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+    STATICFILES_STORAGE = 'dashboard.storage_backends.StaticStorage'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
